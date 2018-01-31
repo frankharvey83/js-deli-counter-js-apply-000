5ccc5b45224eb9c678115ea9c326a4aecd948a2c
Frank H Mcmanus <frank@thefrankharvey.com>
2018-01-31 17:31:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing() {
 if (katzDeliLine.length > 0) {
-  return(`Currently serving ${katzDeliLine[0]}`);
+  return(`Currently serving ${katzDeliLine[0]}.`);
   katzDeliLine.shift();
 } else {
   return "There is nobody waiting to be served!";
